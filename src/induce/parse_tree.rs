46615689d1c75c0aead8f38a1d8b88fc6559baa8
dr0med4r <trompeter-stoll@gmx.de>
2025-04-08 17:05:00 +0200

    tree parsing not completely correct

--- src/induce/parse_tree.rs
+++ src/induce/parse_tree.rs
@@ -1,44 +1,58 @@
 use nom::{
     IResult, Parser,
-    multi::many1,
     branch::alt,
     bytes::complete::is_not,
     character::complete::{char, space0},
+    combinator::map,
+    multi::many1,
     sequence::delimited,
 };
 
-pub enum TreeElement<T> {
-    Leaf{name: T},
-    Node{name: T, children: Vec<TreeElement<T>>},
-}
-
+#[derive(Debug, PartialEq, Eq)]
 pub struct ParseTree<T> {
-    pub item: T,
-    pub children: TreeElement<T>,
+    pub root: T,
+    pub children: Vec<ParseTree<T>>,
 }
 
-fn string(input: &str) -> IResult<&str, &str> {
+impl<'a> From<&'a str> for ParseTree<&'a str> {
+    fn from(val: &'a str) -> Self {
+        ParseTree {
+            root: val,
+            children: vec![],
+        }
+    }
+}
+
+fn atom(input: &str) -> IResult<&str, &str> {
     delimited(space0, is_not(" ()"), space0).parse(input)
 }
 
-fn atom(input: &str) -> IResult<&str, TreeElement<&str>> {
-    let (input,atom) = delimited(space0, is_not(" ()"), space0).parse(input)?;
-    Ok((input, TreeElement::Leaf{name: atom}))
+fn str_to_parsetree_vec(input: &str) -> Vec<ParseTree<&str>> {
+    vec![ParseTree {
+        root: input,
+        children: vec![],
+    }]
 }
 
-fn elements(input: &str) -> IResult<&str, Vec<TreeElement<&str>>> {
+fn elements(input: &str) -> IResult<&str, Vec<ParseTree<&str>>> {
     let (input, elements) = many1(element).parse(input)?;
     Ok((input, elements))
 }
 
-fn element(input: &str) -> IResult<&str, TreeElement<&str>> {
-    let (input, (name, test) ) = delimited(
+fn element(input: &str) -> IResult<&str, ParseTree<&str>> {
+    let (input, (name, elements)) = delimited(
         char('('),
-        (string, alt((elements, atom))),
+        (atom, alt((elements, map(atom, str_to_parsetree_vec)))),
         char(')'),
     )
-    .parse(input)?
-    Ok((input, TreeElement::Node {name, children: test}))
+    .parse(input)?;
+    Ok((
+        input,
+        ParseTree {
+            root: name,
+            children: elements,
+        },
+    ))
 }
 
 // fn parse_element(input: &str) -> IResult<&str, ParseTree<String>> {
@@ -64,7 +78,59 @@ mod tests {
 
     #[test]
     fn element_test() {
-        assert_eq!(element("(ROOT (hi ho))"), Ok(("", "ROOT (hi ho)")));
-        assert_eq!(element("(ROOT (NS (NP hi)))"), Ok(("", "ROOT (NS (NP hi))")));
+        assert_eq!(
+            element("(ROOT hallo)"),
+            Ok((
+                "",
+                ParseTree {
+                    root: "ROOT",
+                    children: vec![ParseTree {
+                        root: "hallo",
+                        children: vec![],
+                    }],
+                }
+            ))
+        );
+        assert_eq!(
+            element("(ROOT (NS hi))"),
+            Ok((
+                "",
+                ParseTree {
+                    root: "ROOT",
+                    children: vec![ParseTree {
+                        root: "NS",
+                        children: vec![ParseTree {
+                            root: "hi",
+                            children: vec![],
+                        }],
+                    },],
+                }
+            ))
+        );
+        assert_eq!(
+            element("(ROOT (NS hi) (NS warum))"),
+            Ok((
+                "",
+                ParseTree {
+                    root: "ROOT",
+                    children: vec![
+                        ParseTree {
+                            root: "NS",
+                            children: vec![ParseTree {
+                                root: "hi",
+                                children: vec![],
+                            }],
+                        },
+                        ParseTree {
+                            root: "NS",
+                            children: vec![ParseTree {
+                                root: "warum",
+                                children: vec![],
+                            }],
+                        }
+                    ],
+                }
+            ))
+        )
     }
 }
