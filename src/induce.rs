7962f6757f4549315fa6bed943672324f92d1e87
dr0med4r <trompeter-stoll@gmx.de>
2025-04-09 15:52:25 +0200

    create pcfg from parse_trees

--- src/induce.rs
+++ src/induce.rs
@@ -9,51 +9,50 @@ pub enum Rhs {
 }
 
 pub fn induce_grammar() -> HashMap<String, HashMap<Rhs, f64>> {
-    let mut a: HashMap<String, HashMap<Rhs, u64>> = HashMap::new();
-    io::stdin().lines().for_each(|line| {
-        if let Ok(line) = line {
-            if let Ok((_, tree)) = parse_tree::element(&line) {
-                // println!("{}", line);
-                tree.execute_for_nodes(&mut |node| {
-                    if node.is_leaf() {
-                        return;
-                    }
-                    let lhs = node.root.to_string();
-                    let rhs: &mut HashMap<Rhs, u64> = match a.get_mut(&lhs) {
-                        Some(rhs) => rhs,
-                        None => {
-                            a.insert(lhs.clone(), HashMap::new());
-                            a.get_mut(&lhs)
-                                .expect("just inserted the value that is asked")
-                        }
-                    };
-                    let Some(child) = node.children.first() else {
-                        return;
-                    };
-                    // assumes that if the child is a leaf it is also a terminal
-                    if child.is_leaf() {
-                        let lhs = Rhs::Terminal(child.root.to_string());
-                        let count = match rhs.get(&lhs) {
-                            Some(&count) => count + 1,
-                            None => 1,
-                        };
-                        rhs.insert(lhs, count);
-                    } else {
-                        let child_names: Vec<String> = node
-                            .children
-                            .iter()
-                            .map(|child| child.root.to_string())
-                            .collect();
-                        let lhs = Rhs::NonTerminals(child_names);
-                        let count = match rhs.get(&lhs) {
-                            Some(&count) => count + 1,
-                            None => 1,
-                        };
-                        rhs.insert(lhs, count);
-                    }
-                });
+    let mut grammar: HashMap<String, HashMap<Rhs, f64>> = HashMap::new();
+    for line in io::stdin().lines() {
+        let Ok(line) = line else { continue };
+        let Ok((_, tree)) = parse_tree::element(&line) else {
+            continue;
+        };
+        // println!("{}", line);
+        tree.execute_for_nodes(&mut |node| {
+            if node.is_leaf() {
+                return;
             }
-        }
-    });
-    todo!()
+            let non_terminal = node.root.to_string();
+            let body: &mut HashMap<Rhs, f64> = match grammar.get_mut(&non_terminal) {
+                Some(rhs) => rhs,
+                None => {
+                    grammar.insert(non_terminal.clone(), HashMap::new());
+                    grammar
+                        .get_mut(&non_terminal)
+                        .expect("just inserted the value that is asked")
+                }
+            };
+            let child = node.children.first().expect("node should not be a leaf");
+            let factor = (body.len() as f64) / (body.len() as f64 + 1.0);
+            // assumes that if the child is a leaf it is also a terminal
+            let lhs = if child.is_leaf() {
+                Rhs::Terminal(child.root.to_string())
+            } else {
+                let child_names: Vec<String> = node
+                    .children
+                    .iter()
+                    .map(|child| child.root.to_string())
+                    .collect();
+                Rhs::NonTerminals(child_names)
+            };
+            let probability: f64 = match body.get(&lhs) {
+                Some(&probability) => probability * factor + 1.0 / (body.len() as f64 + 1.0),
+                None => 1.0 / (body.len() as f64 + 1.0),
+            };
+            for v in (*body).values_mut() {
+                *v *= factor;
+            }
+            body.insert(lhs, probability);
+        });
+    }
+    println!("{:?}", grammar);
+    grammar
 }
