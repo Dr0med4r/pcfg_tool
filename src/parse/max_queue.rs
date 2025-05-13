96993ded781da44c7fb458a281d74a779c2ebdd9
Dr0med4r <trompeter-stoll@gmx.de>
2025-05-13 10:07:53 +0200

    replace radixheap with BTreeMap

--- src/parse/max_queue.rs
+++ src/parse/max_queue.rs
@@ -1,40 +1,66 @@
+use std::{cmp::Ordering, collections::BTreeMap};
+
+use indexmap::{IndexMap, IndexSet};
 use ordered_float::NotNan;
 use radix_heap::RadixHeapMap;
 
 use super::{consequence::Consequence, weight_map::Item};
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct ConsequenceWithoutWeight {
     start: u32,
     item: Item,
     end: u32,
 }
 
-pub struct MaxQueue {
-    heap: RadixHeapMap<NotNan<f64>, usize>,
-    data: Vec<ConsequenceWithoutWeight>,
-    free: Vec<usize>,
+#[derive(PartialEq, Eq)]
+struct Key {
+    weight: NotNan<f64>,
+    idx: usize,
 }
 
-impl Default for MaxQueue {
-    fn default() -> Self {
-        Self {
-            heap: RadixHeapMap::new_at(NotNan::try_from(1.0).unwrap()),
-            data: Default::default(),
-            free: Default::default(),
+impl PartialOrd for Key {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Key {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.weight.cmp(&other.weight) {
+            Ordering::Equal => self.idx.cmp(&other.idx),
+            ord => ord,
         }
     }
 }
 
+#[derive(Default)]
+pub struct MaxQueue {
+    heap: BTreeMap<Key, ()>,
+    data: IndexMap<ConsequenceWithoutWeight, NotNan<f64>, foldhash::fast::RandomState>,
+}
+
+// impl Default for MaxQueue {
+//     fn default() -> Self {
+//         Self {
+//             heap: RadixHeapMap::new_at(NotNan::try_from(1.0).unwrap()),
+//             data: Default::default(),
+//         }
+//     }
+// }
+
 impl MaxQueue {
     pub fn pop(&mut self) -> Option<Consequence> {
-        self.heap.pop().map(|(weight, idx)| {
-            let remaining = &self.data[idx];
-            self.free.push(idx);
+        self.heap.pop_last().map(|(key, _)| {
+            let (remaining, _) = &self
+                .data
+                .get_index(key.idx)
+                .expect("key should be in the map");
             Consequence {
                 start: remaining.start,
                 item: remaining.item,
                 end: remaining.end,
-                weight: *weight,
+                weight: *key.weight,
             }
         })
     }
@@ -46,20 +72,31 @@ impl MaxQueue {
             item: item.item,
             end: item.end,
         };
-        let idx = match self.free.pop() {
-            Some(idx) => {
-                self.data[idx] = part;
-                idx
+        let weight = NotNan::try_from(item.weight).expect("should not be NaN");
+        let (idx, old) = self.data.insert_full(part, weight);
+        if let Some(old) = old {
+            if old < weight {
+                self.heap.remove(&Key { weight: old, idx });
+                self.heap.insert(Key { weight, idx }, ());
             }
-            None => {
-                self.data.push(part);
-                self.data.len() - 1
-            }
-        };
-        self.heap.push(
-            NotNan::try_from(item.weight).expect("should not be NaN"),
-            idx,
-        );
+        } else {
+            self.heap.insert(Key { weight, idx }, ());
+        }
+        // self.heap.entry(Key { weight: old, idx })
+        // let idx = match self.free.pop() {
+        //     Some(idx) => {
+        //         self.data[idx] = part;
+        //         idx
+        //     }
+        //     None => {
+        //         self.data.push(part);
+        //         self.data.len() - 1
+        //     }
+        // };
+        // self.heap.push(
+        //     NotNan::try_from(item.weight).expect("should not be NaN"),
+        //     idx,
+        // );
     }
     pub fn len(&self) -> usize {
         self.data.len()
